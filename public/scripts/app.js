d75c5678f1609bc4c7494d10da89742572109c36
LHY927 <liuhaiyue0927@gmail.com>
2024-03-03 14:01:24 +0100

    initialize pop-up

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,14 @@
 // Client facing scripts here
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector("#add_new_todo_btn");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+showButton.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
