dbb1b7057facdca7f8fe6375957212bf497cea53
LHY927 <liuhaiyue0927@gmail.com>
2024-03-03 15:50:23 +0100

    add item initialized

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,7 @@
 // Client facing scripts here
 const dialog = document.querySelector("dialog");
+const form = document.querySelector("#TODO_form");
+const ongoingItems = document.querySelector(".ongoing_items");
 const showButton = document.querySelector("#add_new_todo_btn");
 const closeButton = document.querySelector("#dialog_cancel_btn");
 const confirmButton = document.querySelector("#dialog_confirm_btn");
@@ -15,14 +17,14 @@ closeButton.addEventListener("click", () => {
 });
 
 // "Confirm" button closes the dialog
-confirmButton.addEventListener("click", (event) => {
+form.addEventListener("submit", (event) => {
     event.preventDefault();
     const titleInput = document.querySelector("#dialog_title_input").value;
     const descriptionInput = document.querySelector("#dialog_description_input").value;
     const dateInput = document.querySelector("#dialog_date_input").value;
     const durationInput = document.querySelector("#dialog_duration_input").value;
     const locationInput = document.querySelector("#dialog_location_input").value;
-    addTODOItem(titleInput, descriptionInput, dateInput, durationInput, locationInput)
+    addTODOItem(0, titleInput, descriptionInput, dateInput, durationInput, locationInput)
     dialog.close();
   });
 
@@ -34,4 +36,6 @@ function addTODOItem(type, name, desctiption, date, duration, location) {
     console.log(date);
     console.log(duration);
     console.log(location);
+    const newItem = ongoingItems.children[0].cloneNode(true);
+    ongoingItems.appendChild(newItem);
 }
