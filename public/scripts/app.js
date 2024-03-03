7d3a7070f90a5742aa24f0cff2b310283127ae19
LHY927 <liuhaiyue0927@gmail.com>
2024-03-03 15:42:10 +0100

    done pop-up format

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 // Client facing scripts here
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("#add_new_todo_btn");
-const closeButton = document.querySelector("dialog button");
+const closeButton = document.querySelector("#dialog_cancel_btn");
+const confirmButton = document.querySelector("#dialog_confirm_btn");
 
 // "Show the dialog" button opens the dialog modally
 showButton.addEventListener("click", () => {
@@ -12,3 +13,25 @@ showButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   dialog.close();
 });
+
+// "Confirm" button closes the dialog
+confirmButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const titleInput = document.querySelector("#dialog_title_input").value;
+    const descriptionInput = document.querySelector("#dialog_description_input").value;
+    const dateInput = document.querySelector("#dialog_date_input").value;
+    const durationInput = document.querySelector("#dialog_duration_input").value;
+    const locationInput = document.querySelector("#dialog_location_input").value;
+    addTODOItem(titleInput, descriptionInput, dateInput, durationInput, locationInput)
+    dialog.close();
+  });
+
+function addTODOItem(type, name, desctiption, date, duration, location) {
+    console.log("Add TODO Item for");
+    console.log(type);
+    console.log(name);
+    console.log(desctiption);
+    console.log(date);
+    console.log(duration);
+    console.log(location);
+}
