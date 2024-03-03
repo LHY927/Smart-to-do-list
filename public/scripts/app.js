804f144f89f7916cadd432a3813b516eda7894c1
LHY927 <liuhaiyue0927@gmail.com>
2024-03-03 16:47:02 +0100

    update item_content part for add item

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -28,14 +28,55 @@ form.addEventListener("submit", (event) => {
     dialog.close();
   });
 
-function addTODOItem(type, name, desctiption, date, duration, location) {
+function addTODOItem(type, name, description, date, duration, location) {
     console.log("Add TODO Item for");
     console.log(type);
     console.log(name);
-    console.log(desctiption);
+    console.log(description);
     console.log(date);
     console.log(duration);
     console.log(location);
     const newItem = ongoingItems.children[0].cloneNode(true);
+    for(const div of newItem.children){
+        console.log(div);
+        if(div.className == "item_icon"){
+            switch(type){
+                //For type, 0 for watch icon, 1 for eat icon, 2 for read icon and 3 for buy icon
+                case 0:
+                    div.children[0].innerText = "live_tv";
+                    break;
+                case 1:
+                    div.children[0].innerText = "restaurant";
+                    break;
+                case 2:
+                    div.children[0].innerText = "auto_stories";
+                    break;
+                case 3:
+                    div.children[0].innerText = "shopping_bag";
+                    break;
+            }
+        }else if(div.className == "item_content"){
+            for(const span of div.children){
+                if(span.className == "item_title"){
+                    span.innerText = name;
+                }else if(span.className == "item_countdown"){
+                    const target = new Date(date);
+                    const today = new Date();
+                    const dateDiff = Math.round((target - today) / (1000 * 3600 * 24))
+                    if(today.getDate() == target.getDate() && today.getMonth() == target.getMonth() && today.getFullYear() == target.getFullYear()){
+                        span.innerText = "Due today";
+                    }else if(dateDiff > 0){
+                        span.innerText = dateDiff + " days left";
+                    }else{
+                        span.innerText = Math.abs(dateDiff) + " days ago";
+                        span.classList.add("item_countdown_past");
+                    }
+                }else if(span.className == "item_description"){
+                    span.innerText = description;
+                }
+            }
+        }
+    }
     ongoingItems.appendChild(newItem);
+    form.reset(); 
 }
