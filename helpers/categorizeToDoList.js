b7ab3389bd0eba0291d2fc80c00ce0ea590374ca
Rey Madrilejo <147770698+rcm68888@users.noreply.github.com>
2024-03-08 17:58:06 -0700

    Update categorizeToDoList.js
    Implemented API to categorize toDoList

--- helpers/categorizeToDoList.js
+++ helpers/categorizeToDoList.js
@@ -1,17 +1,18 @@
-const { addToDoItems, getToDoItemById } = require('./database');
+const { addToDoItems, getToDoItemById } = require('../db/queries/toDoItems');
 const SerpWow = require('google-search-results-serpwow');
 // create the serpwow object
-const serpwow = new SerpWow('491F6EBD87994A119D58ECB26239EDF0');
+const serpwow = new SerpWow(process.env.SerpWowKey);
+const axios = require('axios');
 
 /**
- * 1 - Pre-defined tags contining the keywords used to categorize the task
+ * 1 - Pre-defined tags containing the keywords used to categorize the toDoItem
  * 2 - Building the regex with tags
- * 3 - Combines all regex into object to pass into function to categorize the task
+ * 3 - Combines all regex into object to pass into function to categorize the toDoItem
  */
 // 1
 const watchTags = ['watch', 'movie', 'film', 'directed', 'imdb', 'starring', 'television', 'netflix', 'tv', 'sitcom'];
 const readTags = ['journal', 'textbook', 'read', 'published', 'novel', 'author', 'non-fiction', 'book', 'paperback', 'novels'];
-const eatTags = ['cafe', 'food', 'recipe', 'menu', 'yelp', 'foodora', 'restaurant', 'eat'];
+const eatTags = ['cafe', 'food', 'recipe', 'menu', 'yelp', 'foodora', 'restaurant', 'eat', 'place'];
 // 2
 const watchRegex = buildRegex(watchTags);
 const readRegex = buildRegex(readTags);
@@ -34,7 +35,7 @@ function buildRegex(tags) {
 
 /**
  * Get the user input and filter if there's a verb on that string by using our constructed object
- * @param {String}task The user input
+ * @param {String}toDoItems the user input
  * @return {Boolean}false||{CategoryID}
  */
 const categorizeByVerb = function (toDoItem) {
@@ -44,7 +45,7 @@ const categorizeByVerb = function (toDoItem) {
     eat: { 3: 'restaurants' },
     buy: { 4: 'products' }
   };
-  const key = task.toLowerCase().split(' ');
+  const key = toDoItem.toLowerCase().split(' ');
   if (key.length > 1 && verbs.hasOwnProperty(key[0])) {
     return Number(Object.keys(verbs[key[0]]));
   }
@@ -58,29 +59,37 @@ const categorizeByVerb = function (toDoItem) {
  * @return {[Objects]} what the function returns
  */
 const getSearchResults = (queryString) => {
-  return new Promise(async (resolve, reject) => {
-    let result = await serpwow.json({
+  /*return new Promise( (resolve, reject) => {
+    let result = serpwow.json({
       q: queryString,
       gl: 'ca',
       hl: 'en',
       location: 'Toronto,Ontario,Canada',
       google_domain: 'google.ca',
       num: 3
-    });
-    try {
-      if (!result.organic_results) {
-        resolve(result.local_results);
+    });*/
+  const params = {
+    api_key: serpwow,
+      q: queryString,
+      engine: "google",
+      location: "Toronto,Ontario,Canada",
+      google_domain: "google.ca",
+      gl: "ca",
+      hl: "en",
+      page: "1",
+      num: "3"
+    }
 
-      } else {
-        resolve(result.organic_results);
-      }
-
-    } catch (err) {
-      reject(err.message);
-
+  // make the http GET request to SerpWow
+  axios.get('https://api.serpwow.com/search', { params })
+  .then(response => {
+    if (!result.organic_results) {
+      resolve(result.local_results);
+    } else {
+      resolve(result.organic_results);
     }
   });
-}
+};
 
 /**
  * returns string with all results of API call
@@ -117,18 +126,18 @@ const getCategory = function (string) {
 }
 
 /**
- * Calls addTask function and inserts task into database
- * @param {String} task from user input
+ * Calls addToDoItem function and inserts toDoItem into database
+ * @param {String} toDoItem from user input
  * @return nothing yet..??? maybe something later
  */
 const categorizeToDoItem =  async (obj) => {
-  const { task, user_id } = obj;
+  const { toDoItem, user_id } = obj;
 
-  // check if task already exists for that user
+  // check if toDoItem already exists for that user
   const toDoItems = await getToDoItemById(user_id);
   for (t of toDoItems) {
-    if (t.input.toLowerCase() === task.toLowerCase()) {
-      return { msg : 'Duplicate task. Please try another.'};
+    if (t.input.toLowerCase() === toDoItem.toLowerCase()) {
+      return { msg : 'Duplicate toDoItem. Please try another.'};
 
     }
   }
@@ -144,7 +153,7 @@ const categorizeToDoItem =  async (obj) => {
   if (res) {
     const newToDoItem = await addToDoItem(input);
     return newToDoItem;
-    //calls function that renders new task already categorized in user's main page
+    //calls function that renders new toDoItem already categorized
   } else {
     return getSearchResults(toDoItem)
       .then(res => {
@@ -159,7 +168,7 @@ const categorizeToDoItem =  async (obj) => {
         input.categoryId = res;
         const newToDoItem = addToDoItem(input);
         return newToDoItem;
-
+        console.log(newToDoItem);
       })
       .catch(err => {
         console.error('query error', err.stack);
